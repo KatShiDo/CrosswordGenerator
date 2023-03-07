b86d740cf8d5b71919e7b61997e02295a220b6c1
Nikita <katshido.2003@gmail.com>
2023-03-07 22:28:22 +0300

    Created basic Spring Boot project

---
+++ src/main/java/com/example/crosswordgenerator/CrosswordGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.example.crosswordgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CrosswordGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrosswordGeneratorApplication.class, args);
+    }
+
+}
